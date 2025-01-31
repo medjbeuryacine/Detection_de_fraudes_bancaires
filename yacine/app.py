23223f8fcb8c3c14e0082c82897c820328cd9bbd
medjbeuryacine <medjbeuryacine9@gmail.com>
2025-01-31 12:06:32 +0100

    api

--- yacine/app.py
+++ yacine/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, url_for, redirect, flash, ses
 import pymysql
 import csv
 import io
+import pandas as pd
+import pickle
+
+
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
@@ -71,6 +75,17 @@ def logout():
 
 
 
+# -------------------------------------------------------------------------------MODEL IA-------------------------------------------------------------------------------------
+
+# connecter à la base
+db = init_db()
+cursor = db.cursor()
+
+# ouvrir le model ia
+with open("", "rb") as file: #il manque le model ia
+    model_ia = pickle.load(file)
+
+
 
 
 # page de chargement de fichier et de l'afficher dans la page
@@ -82,8 +97,49 @@ def chargement():
         file = request.files["csvFile"]
         if file:
             stream = io.StringIO(file.stream.read().decode("utf-8"))
-            reader = csv.reader(stream, delimiter=",")
-            table_data = list(reader)
+            data = pd.read_csv(stream, sep=";")
+
+            # selectioner les colonnes
+            colonne = ['transactionId', 'step', 'type', 'amount', 'nameOrig', 'oldbalanceOrg','newbalanceOrig', 'nameDest', 'oldbalanceDest', 'newbalanceDest','isFraud']
+            if not all(col in data.columns for col in colonne):
+                return "Erreur : colonne manquantes dans le fichier csv", 400
+            
+
+            # Sauvegarder la vraie valeur de `isFraud` avant de la supprimer
+            true_fraud = data["isFraud"] if "isFraud" in data.columns else None
+
+            # supprimer la colonne isFraud
+            data = data.drop(columns=["isFraud"])
+            
+            # prediction par ia et les colonne change apartir que les colonnes que le model fait pour predir la fraud ou pas
+            data["isFraud"] = model_ia.predict(data[["amount","oldbalanceOrg","newbalanceDest","type_CASH_IN","type_CASH_OUT","type_DEBIT", "type_PAYMENT","type_TRANSFER"]])
+
+            # Convertir les données en liste pour affichage Jinja
+            table_data = data[["transactionId", "step","type","amount","nameOrig","nameDest","isFraud"]].values.tolist()
+
+            for index, row in data.iterrows():
+                transaction_Id = row["transactionId"]
+                is_fraud_pred = row["isFraud"]
+                true_fraud_value = true_fraud[index] if true_fraud is not None else None
+
+
+
+
+                if true_fraud_value is not None:
+                    prediction_correct = True if true_fraud is not None and is_fraud_pred == true_fraud else False
+                else:
+                    prediction_correct = None
+
+
+
+
+                sql = """
+                        INSERT INTO transactions (transactionId, isFraud, prediction_Correct_Ou_Non) VALUES (%s,%s,%s)
+                      """
+                cursor.execute(sql, (transaction_Id, is_fraud_pred, prediction_correct))
+
+
+            db.commit()
 
     return render_template("chargement.html", table_data=table_data)
 
@@ -95,3 +151,9 @@ def chargement():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+# reader = csv.reader(stream, delimiter=",")
+# table_data = list(reader)
